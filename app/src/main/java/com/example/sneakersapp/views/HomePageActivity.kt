f52975357c6d30c07740bb1d8916fbd119a0f385
hastsinha <hast.sinha@gdn-commerce.com>
2023-12-12 09:04:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sneakersapp/views/HomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sneakersapp.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sneakersapp.R
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
